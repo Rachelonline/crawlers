1908769e04f9aca3265e61564f29712a2ed6eb61
John Rush <john@rush.cool>
2019-10-03 11:31:35 -0700

    paramaterize parsers

--- __app__/adlistingparser/tests/test_ad_listing_parsers.py
+++ __app__/adlistingparser/tests/test_ad_listing_parsers.py
@@ -8,15 +8,21 @@ from __app__.adlistingparser.adlistingparser import parse_ad_listings
 TEST_DATA_LOCATION = "__app__/adlistingparser/tests/ad-listing-tests-data.json"
 TEST_HTML_FOLDER = "__app__/adlistingparser/tests/test-data"
 
+def id_func(item):
+    return item["html"]
 
-def test_site_map_parsers(monkeypatch):
-    # TODO: Parameterize this bad boy
-    with open(TEST_DATA_LOCATION) as test_data_f:
-        test_cases = json.load(test_data_f)
 
-    for test_case in test_cases:
-        with open(os.path.join(TEST_HTML_FOLDER, test_case["html"])) as html:
-            page = html.read()
-        ad_listing_urls, next_urls = parse_ad_listings(test_case["domain"], page)
-        assert ad_listing_urls == test_case["ad-urls"]
-        assert next_urls == test_case["next-urls"]
+def pytest_generate_tests(metafunc):
+    if "test_case" in metafunc.fixturenames:
+        with open(TEST_DATA_LOCATION) as test_data_f:
+            test_cases = json.load(test_data_f)
+            metafunc.parametrize("test_case", [i for i in test_cases], ids=id_func)
+
+
+
+def test_site_map_parsers(test_case):
+    with open(os.path.join(TEST_HTML_FOLDER, test_case["html"])) as html:
+        page = html.read()
+    ad_listing_urls, next_urls = parse_ad_listings(test_case["domain"], page)
+    assert ad_listing_urls == test_case["ad-urls"]
+    assert next_urls == test_case["next-urls"]
--- __app__/sitemapparser/tests/test_site_map_parsers.py
+++ __app__/sitemapparser/tests/test_site_map_parsers.py
@@ -9,13 +9,19 @@ TEST_DATA_LOCATION = "__app__/sitemapparser/tests/site-map-test-data.json"
 TEST_HTML_FOLDER = "__app__/sitemapparser/tests/test-data"
 
 
-def test_site_map_parsers(monkeypatch):
-    # TODO: Parameterize this bad boy
-    with open(TEST_DATA_LOCATION) as test_data_f:
-        test_cases = json.load(test_data_f)
+def id_func(item):
+    return item["html"]
 
-    for test_case in test_cases:
-        with open(os.path.join(TEST_HTML_FOLDER, test_case["html"])) as html:
-            page = html.read()
-        ad_listing_urls = parse_ad_listing_page(test_case["domain"], page)
-        assert ad_listing_urls == test_case["ad-listing-urls"]
+
+def pytest_generate_tests(metafunc):
+    if "test_case" in metafunc.fixturenames:
+        with open(TEST_DATA_LOCATION) as test_data_f:
+            test_cases = json.load(test_data_f)
+            metafunc.parametrize("test_case", [i for i in test_cases], ids=id_func)
+
+
+def test_site_map_parsers(test_case):
+    with open(os.path.join(TEST_HTML_FOLDER, test_case["html"])) as html:
+        page = html.read()
+    ad_listing_urls = parse_ad_listing_page(test_case["domain"], page)
+    assert ad_listing_urls == test_case["ad-listing-urls"]
