18c9eb36bf94e540f41bbc6ae9cc3c8c657d49c5
John Rush <john@rush.cool>
2019-10-21 21:00:57 -0700

    cleanup and metrics

--- __app__/processor/__init__.py
+++ __app__/processor/__init__.py
@@ -1,7 +1,17 @@
 import logging
 import json
 import azure.functions as func
+from __app__.utils.metrics.metrics import get_client, enable_logging
 
 def main(inmsg: func.ServiceBusMessage, doc: func.Out[func.Document]) -> None:
-    #message = json.loads(inmsg.get_body().decode("utf-8"))
+    azure_tc = get_client()
+    enable_logging()
+
+    message = json.loads(inmsg.get_body().decode("utf-8"))
     doc.set(func.Document.from_json(inmsg.get_body()))
+
+    azure_tc.track_metric(
+        "ad-processed", 1, properties={"domain": message["domain"]}
+    )
+    azure_tc.flush()
+
