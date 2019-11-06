62f5be90c61c18c4664c302550f1416f9b93a356
Nhu Nguyen <nhu313@gmail.com>
2019-11-05 19:11:14 -0800

    added unicode when file is opened to work with windows

--- __app__/sitemapparser/tests/test_site_map_parsers.py
+++ __app__/sitemapparser/tests/test_site_map_parsers.py
@@ -15,13 +15,13 @@ def id_func(item):
 
 def pytest_generate_tests(metafunc):
     if "test_case" in metafunc.fixturenames:
-        with open(TEST_DATA_LOCATION) as test_data_f:
+        with open(TEST_DATA_LOCATION, encoding="utf8") as test_data_f:
             test_cases = json.load(test_data_f)
             metafunc.parametrize("test_case", [i for i in test_cases], ids=id_func)
 
 
 def test_site_map_parsers(test_case):
-    with open(os.path.join(TEST_HTML_FOLDER, test_case["html"])) as html:
+    with open(os.path.join(TEST_HTML_FOLDER, test_case["html"]), encoding="utf8") as html:
         page = html.read()
     ad_listing_urls = parse_ad_listing_page(test_case["domain"], page)
     assert ad_listing_urls == test_case["ad-listing-urls"]
