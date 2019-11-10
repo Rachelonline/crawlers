66149e5c4e2ce1f331defb50366c6957436155d9
maxzzze <maliapoulios@nyu.edu>
2019-11-10 10:56:39 -0500

    move to processors, add tests

--- __app__/processor/__init__.py
+++ __app__/processor/__init__.py
@@ -1,13 +1,39 @@
+import os
 import logging
 import json
 import azure.functions as func
 from __app__.utils.metrics.metrics import get_client, enable_logging
+from __app__.processor.scorers.cache import RedisCache
+from __app__.processor.adscorer import score_ad
 
-def main(inmsg: func.ServiceBusMessage, doc: func.Out[func.Document]) -> None:
+
+def main(
+    inmsg: func.ServiceBusMessage, doc: func.Out[func.Document], sdoc: func.Out[func.Document]) -> None:
+    CACHE = RedisCache()
+
+    DEFAULT_FUNCTIONS = {
+        "frequency": {
+            "attribute_list": [
+                "age",
+                "location",
+                "gender"
+            ],
+            "cache": CACHE
+        },
+        "twilio": {
+            "account_sid": os.environ["account_sid"],
+            "auth_token": os.environ["auth_token"],
+            "cache": CACHE
+        }
+    }
     azure_tc = get_client()
     enable_logging()
 
     message = json.loads(inmsg.get_body().decode("utf-8"))
+    score_msg = score_ad(message, functions=DEFAULT_FUNCTIONS)
+    if score_msg:
+        sdoc.set(func.Document.from_json(score_msg))
+
     doc.set(func.Document.from_json(inmsg.get_body()))
 
     azure_tc.track_metric(
