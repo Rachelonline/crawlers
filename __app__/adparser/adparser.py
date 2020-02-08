bb8a50e13cf6461abb75cd495f6e65014f926c61
John Rush <john@rush.cool>
2020-02-08 06:27:47 -0800

    adparser for gfemonkey.com

--- __app__/adparser/adparser.py
+++ __app__/adparser/adparser.py
@@ -7,8 +7,12 @@ from __app__.adparser.sites.escortdirectory import EscortDirectory
 from __app__.adparser.sites.megapersonals_eu import MegaPersonals
 from __app__.adparser.sites.vipgirlfriend_com import VIPGirlfriend
 from __app__.adparser.sites.twobackpage_com import TwoBackpage
+<<<<<<< HEAD
 from __app__.adparser.sites.onebackpage_com import OneBackPage_com
 from __app__.adparser.sites.bedpage_com import BedPage_com
+=======
+from __app__.adparser.sites.gfemonkey_com import GfeMonkey
+>>>>>>> adparser for gfemonkey.com
 from __app__.utils.ads.adstore import get_ad_page
 from __app__.utils.metrics.metrics import get_client, enable_logging
 
@@ -21,6 +25,7 @@ AD_PARSERS = {
     "2backpage.com": TwoBackpage,
     "onebackpage.com": OneBackPage_com,
     "bedpage.com": BedPage_com,
+    "gfemonkey.com": GfeMonkey
 }
 
 # {'ad-page-blob': 'https://picrawling.blob.core.windows.net/test-ads/2019/09/11/cityxguide.com/9675015e-858e-48d1-97c6-d7d3a8bcf74f', 'domain': 'cityxguide.com', 'metadata': {'domain': 'cityxguide.com', 'manual-job': True, 'ad-listing-crawled': '2019-09-10T17:04:02', 'ad-crawled': '2019-09-11T14:57:39'}##}
---
+++ __app__/adparser/sites/gfemonkey_com.py
@@ -0,0 +1,125 @@
+from urllib.parse import urljoin
+from datetime import datetime
+from typing import List
+from __app__.adparser.sites.base_ad_parser import BaseAdParser
+
+
+def decode_email(encoded_email: str) -> str:
+    """ decode the alleged email protection"""
+
+    def sub_str_to_int(string, index):
+        return int(string[index : index + 2], 16)
+
+    factor = sub_str_to_int(encoded_email, 0)
+    index = 2
+    email = ""
+
+    while index < len(encoded_email):
+        email += chr(sub_str_to_int(encoded_email, index) ^ factor)
+        index += 2
+    return email
+
+
+class GfeMonkey(BaseAdParser):
+    gender_lookup = {"female-escorts": "female", "male-escorts": "male", "ts": "trans"}
+
+    def primary_phone_number(self) -> str:
+        contact = self.soup.find("div", id="contact")
+        if contact:
+            phone = contact.find("td", text="Phone:")
+            if phone:
+                return phone.next_sibling.text
+
+    def phone_numbers(self) -> List:
+        phone_numbers_found = []
+        prim_phone_number = self.primary_phone_number()
+        if prim_phone_number:
+            phone_numbers_found.append(prim_phone_number)
+        matches = self.phone_re.findall(self.ad_text())
+        phone_numbers_found.extend(["".join(match) for match in matches])
+        return phone_numbers_found
+
+    def date_posted(self) -> str:
+        # GFE doesn't have ad posted dates
+        return None
+
+    def name(self) -> str:
+        return self.ad_title()
+
+    def primary_email(self) -> str:
+        contact = self.soup.find("div", id="contact")
+        if contact:
+            email = contact.find("td", text="Email:")
+            if email:
+                email_link = email.next_sibling.find("span")
+                email_link = email_link.get("data-cfemail")
+                if email_link:
+                    return decode_email(email_link)
+
+    def emails(self) -> List:
+        emails_found = []
+        matches = self.email_re.findall(self.ad_text())
+        emails_found.extend(["".join(match) for match in matches])
+        emails_found.append(self.primary_email())
+        return emails_found
+
+    def social(self) -> List:
+        return None
+
+    def age(self) -> str:
+        stats = self.soup.find("div", id="stats")
+        if stats:
+            stat = stats.find("td", text="Age:").next_sibling
+            return stat.text
+
+    def image_urls(self) -> List:
+        urls = []
+        image_div = self.soup.find("div", id="carousel")
+        if image_div:
+            images = image_div("img", src=True, class_=False) or []
+            for image in images:
+                urls.append(urljoin("https://www.gfemonkey.com", image["src"]))
+
+        return urls
+
+    def location(self) -> str:
+        stats = self.soup.find("div", id="stats")
+        if stats:
+            stat = stats.find("td", text="Location:").next_sibling
+            return stat.text
+
+    def ethnicity(self) -> str:
+        stats = self.soup.find("div", id="stats")
+        if stats:
+            stat = stats.find("td", text="Ethnicity:").next_sibling
+            return stat.text
+
+    def gender(self) -> str:
+        stats = self.soup.find("div", id="stats")
+        if stats:
+            stat = stats.find("td", text="Gender:").next_sibling
+            return stat.text
+
+    def services(self) -> List:
+        return None
+
+    def website(self) -> str:
+        contact = self.soup.find("div", id="contact")
+        if contact:
+            phone = contact.find("td", text="Website:")
+            if phone:
+                return phone.next_sibling.text.strip()
+
+    def ad_text(self) -> str:
+        content = "\n".join(
+            string
+            for string in self.soup.find("div", id="pageContent").stripped_strings
+        )
+        return content.replace(u"\xa0", u" ")
+
+    def ad_title(self) -> str:
+        title = self.soup.find("h2", class_="profile-name")
+        return title.text
+
+    def orientation(self) -> str:
+        return None
