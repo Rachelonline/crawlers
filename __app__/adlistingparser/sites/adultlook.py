6982a7a5982321d08a3fe27451cd51796e0faf61
ja-b <jacob.bennett29@gmail.com>
2020-06-23 17:50:38 -0500

    chore: clean up

--- __app__/adlistingparser/sites/adultlook.py
+++ __app__/adlistingparser/sites/adultlook.py
@@ -1,5 +1,6 @@
 from __app__.adlistingparser.sites.base_adlisting_parser import BaseAdListingParser, AdListing
 
+
 class AdultLook(BaseAdListingParser):
     def ad_listings(self):
         listings = self.soup.select("#results > div > div:nth-child(2) > div:nth-child(1) > div")
