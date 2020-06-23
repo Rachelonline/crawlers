3569bb428484b8b3f963589be895ad5635ac4ffb
nicholas.michalesko <nicholas.michalesko@transmarketgroup.com>
2020-06-23 18:20:03 -0500

    adding some test data

---
+++ __app__/adparser/sites/usaadultclassified_nl.py
@@ -0,0 +1,50 @@
+[
+    {
+        "html": "20200623-usaadultclassified_nl1.html",
+        "domain": "usaadultclassified.nl",
+        "metadata": {},
+        "ad-data": {
+            "primary-phone-number": "+1-3476675191",
+            "phone-numbers": ["+1-3476675191"],
+            "image-urls": [
+                "https://ella.fotki.ch/20200430/1592/3884/8420/0524/delaware-female-escorts_3799975_0496220-m.jpeg",
+                "https://ella.fotki.ch/20200430/1592/3810/8757/9492/delaware-female-escorts_3799975_0532481-m.jpeg",
+                "https://ella.fotki.ch/20200430/1592/3896/7274/7376/delaware-female-escorts_3799975_0570980-m.jpeg"
+            ],
+            "location": "Delaware",
+            "ad-text": "🌟Exotic+13476675191playmate for Generous Gentlemen that want to escape into the erotic pleasures of their fantasies! Relax and unwind in my upscale hosting suite or we can have the ultimate erotic adventure in the comfort of your home! I am also available for CarPlay...I am an Independent provider so you'll never worry of unsafe situations and our time together will be discreet and uninterrupted. Your satisfaction is always my priority 💋13476675191",
+            "ad-title": "🆕️🌹Speacial All Day|5🌟13476675191 EroticPlaymate🌟Luxury Location 🚫Rush🌟13476675191",
+            "name": "Roseline  🌟",
+            "services": [
+                  "Breast relief / Russian",
+                  "Deep throat",
+                  "Domination - mild (BDSM)",
+                  "Domination - severe",
+                  "Face sitting",
+                  "Fantasy outfits (on request)",
+                  "Girlfriend experience (GFE)",
+                  "Intercourse - Anal (Greek)",
+                  "Intercourse - Oral",
+                  "Intercourse - Vaginal (FS)",
+                  "Kissing - closed lips",
+                  "Kissing - deep (DFK)",
+                  "Lunch / dinner dates",
+                  "Massage - sensual",
+                  "Massage - therapeutic",
+                  "Multiple sessions within date time (MSOG)",
+                  "Oral - CIM",
+                  "Oral - receiving (DATY)",
+                  "Oral - without condom (BBBJ)",
+                  "Pornstar experience (PSE)",
+                  "Prostate massage",
+                  "Rimming - receiving",
+                  "Role-playing",
+                  "Service by two providers",
+                  "Submission",
+                  "Toy show",
+                  "Travel / extended dates",
+                  "Water sports - giving"
+            ]
+        }
+    }
+]
--- __app__/sitemapparser/sites/usaadultclassified_nl.py
+++ __app__/sitemapparser/sites/usaadultclassified_nl.py
@@ -3,6 +3,4 @@ from bs4 import BeautifulSoup
 
 
 def usaadultclassified_nl(html):
-    # TODO: this is actually for adparser
-    soup = BeautifulSoup(html, "html.parser")
     return [x['href'] for x in soup.find_all("a", class_="posttitle")]
