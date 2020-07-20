c636512582da9b8108fdcb3a3463cd0f744dce4b
Pete Dunlap <pete@seattleagainstslavery.org>
2020-07-20 09:49:03 -0500

    gets total working

--- tools/queues/cleaner.py
+++ tools/queues/cleaner.py
@@ -16,16 +16,16 @@ def clear_queue(queue_name: str, domain: str, variant: str) -> None:
     receive = {"active": queue.get_receiver, "deadletter": queue.get_deadletter_receiver}
     with receive[variant](
         prefetch=200, mode=1, idle_timeout=5
-        # mode=2 means it will delete on receive. We generally want that for perfomance
+        # mode=2 means it will delete on receive. We generally want that for performance
         # but if you want to look at the messages set mode=1 (Peek)
     ) as receiver:
         for msg in receiver:
             msg_data = json.loads(next(msg.body).decode("utf8"))
             if domain in msg_data["domain"]:
                 try:
-                    msg.complete()
-                    print(msg_data)
                     total += 1
+                    msg.complete()
                 except MessageAlreadySettled:
                     pass
+
     print(f"END: Removed {total} messages from {queue_name} for {domain}")
